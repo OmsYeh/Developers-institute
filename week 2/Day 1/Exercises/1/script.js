c800358a98df12dc983abf1945f38d6496fceb63
omsyeh <omri.yehudai97@gmail.com>
2020-06-21 11:34:28 +0300

    creating week 2 day 1 and adding the first files

---
+++ week 2/Day 1/Exercises/1/script.js
@@ -0,0 +1,12 @@
+let addressNumber = 22 ;
+let addressStreet = " haavoda ";
+let country = "israel ";
+
+let global_address = addressNumber + addressStreet + country ;
+
+let birthyear = 1997;
+
+let infiveyrs = 2025;
+let futureAge = (infiveyrs - birthyear);
+console.log(futureAge);
+
