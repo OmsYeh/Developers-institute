6e4965a12b8f672a938b0769818051d29ae299ee
omsyeh <omri.yehudai97@gmail.com>
2020-06-22 10:43:51 +0300

    push 10:43/2

--- week 2/Day 2/Class work/Exercise 1/Script.js
+++ week 2/Day 2/Class work/Exercise 1/Script.js
@@ -3,7 +3,7 @@ let legalAge=18
 let age=prompt("What is your current age?");
 
 
-if (age == 18){
+if (age === 18){
     alert("Congratulations on your first year of driving. Enjoy the ride!")
 } else if (age > 18){
   alert("Powering On. Enjoy the ride!")
