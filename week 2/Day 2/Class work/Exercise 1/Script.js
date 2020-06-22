a2c524d64c9d83d527ef2b90ff7457bcf731b736
omsyeh <omri.yehudai97@gmail.com>
2020-06-22 10:40:50 +0300

    test pushh

---
+++ week 2/Day 2/Class work/Exercise 1/Script.js
@@ -0,0 +1,12 @@
+let legalAge=18
+
+let age=prompt("What is your current age?");
+
+
+if (age == 18){
+    alert("Congratulations on your first year of driving. Enjoy the ride!")
+} else if (age > 18){
+  alert("Powering On. Enjoy the ride!")
+} else{
+  alert("Sorry, you are too young to drive this car. Powering off")
+}
