7b3829b5577a7f097e601ff7d87a7640f71d7bc0
omsyeh <omri.yehudai97@gmail.com>
2020-06-23 08:51:46 +0300

    morning push 23/06/2020

---
+++ week 2/Day 1/Exercises/Exercise_XP_Ninja/script.js
@@ -0,0 +1,31 @@
+// Exercise 1
+
+5 >= 1      //==> true
+ 0 === 1      //==> false
+ 4 <= 1     //==> false
+ 1 != 1     //==> false
+ "A" > "B"     //==> true
+ "B" < "C"      //==> false
+ "a" > "A"      //==> true
+ "b" < "A"      //==> true
+ true === false     //==> false
+ true != true     //==> flase
+
+ // Exercise 2
+
+ let c;     //==> undefined
+ let a = 34;      //==> defining a var
+ let b = 21;      //==> defining a var
+ a = 2;     //==> redefining 'a'
+ a + b      //==> result will be 23
+
+ What will return a + b? //result will be 23
+ What is the variable c equal to? //undefined
+ Analyse the code below without running it, what will be the output ?
+ console.log(3 + 4 + '5'); //result will be 12
+
+//I ran the code and noted that my predicted result is incorrect -
+//the correct result is "75" because (3+4)=7 and '5' remains to be a string
+//so the contactecation of the 7 and '5' = '75'.
+
+// Exercise 3
