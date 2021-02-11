f0756ac8601c4dd4cb28aa12e3524a2c1ccf7953
OmsYeh <Omri.yehudai97@gmail.com>
2021-02-11 09:59:32 +0200

    week 5 day 1

--- week_5/Day1/Exercises/ExerciseXP/XP.py
+++ week_5/Day1/Exercises/ExerciseXP/XP.py
@@ -1,62 +1,64 @@
+import string
 # Exercise 1:
 
 #
-class Cat:
-    species = 'mammal'
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    @staticmethod
-    def oldest_cat(*cats):
-        oldest = 0
-        cats = list(cats)
-        for cat in cats:
-            if cat.age > oldest:
-                oldest = cat.age
-        return f'The Oldest cat is {oldest} years old'
-
-
-my_cat = Cat("Corey", 1)
-gila_cat = Cat("Charlie", 7)
-tzios_cat = Cat("Niki", 8)
-
-Cat.oldest_cat(my_cat, gila_cat, tzios_cat)
+# class Cat:
+#     species = 'mammal'
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     @staticmethod
+#     def oldest_cat(*cats):
+#         oldest = 0
+#         cats = list(cats)
+#         for cat in cats:
+#             if cat.age > oldest:
+#                 oldest = cat.age
+#         return f'The Oldest cat is {oldest} years old'
+#
+#
+# my_cat = Cat("Corey", 1)
+# gila_cat = Cat("Charlie", 7)
+# tzios_cat = Cat("Niki", 8)
+#
+# Cat.oldest_cat(my_cat, gila_cat, tzios_cat)
 
 #
 # # Exercise 2
+
+
+# class Dog:
+#     def __init__(self, name_dog, height_dog):
+#         self.name = name_dog
+#         self.height = height_dog
 #
-class Dog:
-    def __init__(self, name_dog, height_dog):
-        self.name = name_dog
-        self.height = height_dog
-
-    def talk(self):
-        return 'Woof'
-
-    def jump(self):
-        return f'{self.name} can jump {self.height *2} meters high'
-
-
-davids_dog = Dog("Rex", 0.50)
-davids_dog.jump()
-davids_dog.talk()
-sarahs_dog = Dog('Teacup', 0.20)
-sarahs_dog.jump()
-sarahs_dog.talk()
-
-
-def tallest_dog(*dogs):
-    tallest = 0
-    dogs = list(dogs)
-    for dog in dogs:
-        if dog.height > tallest:
-            tallest = dog.height
-            return f'The tallest dog is {tallest} meters tall'
-
-
-tallest_dog(davids_dog, sarahs_dog)
+#     def talk(self):
+#         return 'Woof'
+#
+#     def jump(self):
+#         return f'{self.name} can jump {self.height *2} meters high'
+#
+#
+# davids_dog = Dog("Rex", 0.50)
+# davids_dog.jump()
+# davids_dog.talk()
+# sarahs_dog = Dog('Teacup', 0.20)
+# sarahs_dog.jump()
+# sarahs_dog.talk()
+#
+#
+# def tallest_dog(*dogs):
+#     tallest = 0
+#     dogs = list(dogs)
+#     for dog in dogs:
+#         if dog.height > tallest:
+#             tallest = dog.height
+#             return f'The tallest dog is {tallest} meters tall'
+#
+#
+# tallest_dog(davids_dog, sarahs_dog)
 
 # Exercise 3
 # Define a class called Song, it will show the lyrics of a song.
@@ -71,18 +73,18 @@ tallest_dog(davids_dog, sarahs_dog)
 
 # Call the sing_me_song method on this variable.
 
+
+# class Song:
+#     def __init__(self, lyrics):
+#         self.lyrics = lyrics
 #
-class Song:
-    def __init__(self, lyrics):
-        self.lyrics = lyrics
-
-    def sing_me_a_song(self):
-        for strip in self.lyrics:
-            print(strip)
-
-
-happy_bday = Song(["Have a sunshine on you,", "Happy Birthday to you !"])
-happy_bday.sing_me_a_song()
+#     def sing_me_a_song(self):
+#         for strip in self.lyrics:
+#             print(strip)
+#
+#
+# stairway = Song(["There’s a lady who's sure", "all that glitters is gold", "and she’s buying a stairway to heaven"])
+# stairway.sing_me_a_song()
 
 
 # Exercise 4
@@ -97,12 +99,14 @@ class Zoo:
     def __init__(self, zoo_name):
         self.animals = []
         self.name = zoo_name
+        self.pens = {}
 
     def add_animal(self, new_animal):
-        if new_animal in self.animals:
+        lower = new_animal.lower()
+        if lower in self.animals:
             print('Animal already exists')
         else:
-            self.animals.append(new_animal)
+            self.animals.append(lower)
 
     def get_animals(self):
         print(self.animals)
@@ -116,24 +120,22 @@ class Zoo:
 
     def sort_animal(self):
         self.animals.sort()
+        print(self.animals)
 
-    def sorted_to_dict(self):
-        alpha_list = []
-        for i in range(len(self.animals)):
-            alpha_list.append(i)
-        print(alpha_list)
+# Exercise 4 point 6
+# couldn't figure this one out at all.
 
 
-my_zoo = Zoo('Omris Zoo')
+my_zoo = Zoo("Omri's Zoo")
 my_zoo.add_animal("cheetah")
 my_zoo.add_animal("zebra")
 my_zoo.add_animal("monkey")
 my_zoo.add_animal("fish")
 my_zoo.add_animal("Lion")
 
-my_zoo.get_animals()
-my_zoo.sell_animal('Lion')
-my_zoo.get_animals()
+# my_zoo.get_animals()
+# my_zoo.sell_animal('Lion')
+# my_zoo.get_animals()
 
 my_zoo.sort_animal()
-my_zoo.sorted_to_dict()
+
--- week_5/week5_prac.py
+++ week_5/week5_prac.py
@@ -6,23 +6,32 @@
 # Is when data and methods are bundled together
 
 
-class Person:
+# class Person:
 
-    def __init__(self, name, age=0):
-        self.name = name
-        self.age = age
-        self.skills = []
+    # def __init__(self, name, age=0):
+    #     self.name = name
+    #     self.age = age
+    #     self.skills = []
 
-    def say_hello(self):
-        print(f"Hi, my name is {self.name} I'm {self.age} years old.")
-        print("I'm good at", ",".join(self.skills))
-
-    def have_birthday(self):
-        self.age += 1
-
-    def learn_something(self, thing):
-        self.skills.append(thing)
+#     def say_hello(self):
+#         print(f"Hi, my name is {self.name} I'm {self.age} years old.")
+#         print("I'm good at", ",".join(self.skills))
+#
+#     def have_birthday(self):
+#         self.age += 1
+#
+#     def learn_something(self, thing):
+#         self.skills.append(thing)
 
 
 # All methods must have self as param 1.
 # init... attach variable to self.
+
+
+# PYTHON FILE I/O
+# with open("blah.txt", "w") as f:
+#     f.write("hello \n")
+#
+# with open("blah.txt", "a") as f:
+#     f.write("my name is omri")
+
